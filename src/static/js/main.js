ec1d91ab9d24d7929014ddbb96c24ee3460f0cec
andysellick <andysellick@builtvisible.com>
2016-09-12 16:18:55 +0100

    Initial commit

---
+++ src/static/js/main.js
@@ -0,0 +1,47 @@
+/* globals angular */
+
+//angular.module('peddler', []).controller('peddlerController',function($scope,$http,$window,$timeout,$compile){
+angular.module('peddler', []).controller('peddlerController',function() {
+	this.obj = {
+		'clicks':0, //how many times clicked
+		'pedals':0, //based on times clicked and extras
+		'distance':0, //total distance covered, based on pedals
+	};
+
+	//on load, check localstorage for previous save
+	this.init = function(){
+		var saved = localStorage.getItem('peddler');
+		console.log(saved);
+		if(saved.length){
+			console.log('loading');
+			saved = JSON.parse(saved);
+			this.obj = saved;
+		}
+
+		$timeout(function(){
+			console.log('timeout done');
+		},2000);
+		
+	};
+
+	//save all data to local storage
+	this.save = function(){
+		console.log('saving');
+		localStorage.setItem('peddler', JSON.stringify(this.obj));
+	};
+
+	//clear localstorage
+	this.deleteSave = function(){
+		console.log('delete');
+		localStorage.setItem('peddler', '');
+	};
+
+	this.clicker = function(){
+		this.obj.clicks++;
+		console.log('click');
+	};
+	
+	this.loop = function(){
+
+	};
+});
