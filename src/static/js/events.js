57f6ba1cbe76a363abd921577a1b469840c357eb
andysellick <andysellick@builtvisible.com>
2016-12-09 15:24:13 +0000

    Grudging frippery

--- src/static/js/events.js
+++ src/static/js/events.js
@@ -2,6 +2,18 @@
 var events = [
 {	'name':'Flat tyre',
 	'message':'You got a puncture.',
-	'penalty':-20
+	'speedpenalty':-20,
+
 },
 ];
+
+/*
+	possible events/interactions
+	
+	puncture/fix puncture
+	random achievement related things e.g. take photo
+	be injured/stop at a hospital
+	change tires/other bike gear - might need varying terrain, weather
+	how about if you're actually running the game you go faster? or need less rests?
+
+*/
